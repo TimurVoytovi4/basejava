eee9c0f7b0160bb673259c93eb6b7e281cf1df96
timur <t.voytovi4@gmail.com>
2017-11-25 10:03:39 +0200

    Сделано "0"-е домашнее задание

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,71 @@
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static int j = 0;
+    Resume[] storage = new Resume[10];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++)
+            storage[i] = null;
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                return;
+            }
+        }
     }
 
+
     Resume get(String uuid) {
-        return null;
+        Resume r = new Resume();
+        for (int i = 0; i < storage.length; i++)
+            if (storage[i] != null && storage[i].uuid == uuid) {
+            r = storage[i];
+            return r;
+            }
+        return r;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++)
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                storage[i] = null;
+                return;
+            }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null){
+                j++;
+            }
+        }
+        Resume [] res = new Resume[j];
+        int k = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                res[k] = storage[i];
+                k++;
+            }
+        }
+        j = 0;
+        return res;
     }
 
+
     int size() {
-        return 0;
+        int k = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) k++;
+        }
+        return k;
     }
 }
