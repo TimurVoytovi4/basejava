5b511a6fca17547641dde06fa8aa532989577083
timur <t.voytovi4@gmail.com>
2017-12-13 05:08:06 +0200

    Added:  1)update()  2)verification in update/delete/save  3)overflow check in save()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,44 +9,65 @@ public class ArrayStorage {
     void clear() {
         for (int i = 0; i < size; i++)
             storage[i] = null;
-            size = 0;
+        size = 0;
+    }
+
+    void update(Resume r, Resume q) {
+        if (r.uuid != null) {
+            for (int i = 0; i < size--; i++) {
+                if (r.uuid.equals(storage[i].uuid)) {
+                    storage[i] = q;
+                    System.out.println("Успешно обновлено");
+                    return;
+                }
+            }
+            System.out.println("Ошибка:нет совпадений");
+        }
     }
 
     void save(Resume r) {
         if (r.uuid != null) {
-            storage[size] = r;
+            for (int i = 0; i < size--; i++) {
+                if (r.uuid.equals(storage[i].uuid)) {
+                    System.out.println("Ошибка:такая запись уже сушествует");
+                    return;
+                }
+            }
+            if (size++ <= storage.length)
+                storage[size] = r;
             size++;
         }
     }
 
     Resume get(String uuid) {
         Resume r = new Resume();
-        for (int i = 0; i < size -1; i++)
+        for (int i = 0; i < size--; i++)
             if (storage[i].uuid.equals(uuid)) {
-            r = storage[i];
-            }else r = null;
+                r = storage[i];
+            } else r = null;
         return r;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size--; i++)
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
                 storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
                 return;
             }
-        }
+    }
+
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume [] res = new Resume[size];
-        System.arraycopy(storage, 0,res,0,size);
+        Resume[] res = new Resume[size];
+        System.arraycopy(storage, 0, res, 0, size);
         return res;
     }
 
     int size() {
-       return size;
+        return size;
     }
 }
