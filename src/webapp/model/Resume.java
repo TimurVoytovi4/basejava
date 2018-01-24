d63b53c463dcbb50320839bc79fab6c30e93f172
TimurVoytovi4 <t.voytovi4@gmail.com>
2018-01-24 11:17:57 +0200

    Refactored Resume

--- src/webapp/model/Resume.java
+++ src/webapp/model/Resume.java
@@ -38,17 +38,19 @@ public class Resume {
                 '}';
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Resume resume = (Resume) o;
-        return Objects.equals(uuid, resume.uuid);
+        return Objects.equals(uuid, resume.uuid) &&
+                Objects.equals(fullName, resume.fullName);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(uuid);
+        return Objects.hash(uuid, fullName);
     }
 }
