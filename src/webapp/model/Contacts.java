0845ed0072b447aa240a1e118158908986c24213
TimurVoytovi4 <t.voytovi4@gmail.com>
2018-02-13 13:59:35 +0200

    Created object contacts

--- src/webapp/model/Contacts.java
+++ src/webapp/model/Contacts.java
@@ -1,7 +1,16 @@
 package webapp.model;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Contacts {
+    private Map<String, String> contactTable = new HashMap<>();
 
+    public void setContacts(String idContact, String valContact) {
+        contactTable.put(idContact, valContact);
+    }
+
+    public Map<String, String> getContactTable() {
+        return contactTable;
+    }
 }
