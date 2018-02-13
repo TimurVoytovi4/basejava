8594abbc4b81f3e4d24ee76bc49a1bb22719c75a
TimurVoytovi4 <t.voytovi4@gmail.com>
2018-02-13 15:21:49 +0200

    An object is implemented that implements the last two sections of the resume

--- src/webapp/model/DateTextItems.java
+++ src/webapp/model/DateTextItems.java
@@ -1,4 +1,30 @@
 package webapp.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DateTextItems {
+    private List<PlaceOfStay> list = new LinkedList<>();
+
+    public void setList(PlaceOfStay obj) {
+        list.add(obj);
+    }
+
+    public List<PlaceOfStay> getList() {
+        return list;
+    }
+
+    class PlaceOfStay {
+        String namePlace;
+        String date;
+        String position;
+        String description;
+
+        public PlaceOfStay(String namePlace, String date, String position, String description) {
+            this.namePlace = namePlace;
+            this.date = date;
+            this.position = position;
+            this.description = description;
+        }
+    }
 }
