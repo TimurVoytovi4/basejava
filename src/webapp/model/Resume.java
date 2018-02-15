48198c8948a6e52dd5ad1df61041467b606dcdb6
TimurVoytovi4 <t.voytovi4@gmail.com>
2018-02-15 23:48:57 +0200

    Added getter & setter

--- src/webapp/model/Resume.java
+++ src/webapp/model/Resume.java
@@ -1,5 +1,6 @@
 package webapp.model;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -23,12 +24,63 @@ public class Resume implements Comparable<Resume> {
     private DateTextItems experience = new DateTextItems(SectionType.EXPERIENCE);
     private DateTextItems education = new DateTextItems(SectionType.EDUCATION);
 
-    public void setContacts(Contacts id,Contacts value) {
-        contacts.put(id,value);
+    public void setContacts(Contacts id, Contacts value) {
+        contacts.put(id, value);
     }
 
-    public Map<Contacts, Contacts> getContacts(){
+    public Map<Contacts, Contacts> getContacts() {
         return contacts;
+    }
+
+    public TextField getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(String text) {
+        personal.setTextContent(text);
+    }
+
+    public TextField getObjective() {
+        return objective;
+    }
+
+    public void setObjective(String text) {
+        objective.setTextContent(text);
+    }
+
+
+    public TextItem getAchievement() {
+        return achievement;
+    }
+
+    public void setAchievement(String text) {
+        achievement.setList(text);
+    }
+
+    public TextItem getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String text) {
+        qualification.setList(text);
+    }
+
+    public DateTextItems getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String namePlace, LocalDate start, LocalDate end, String position, String description) {
+        DateTextItems.PlaceOfStay obj = new DateTextItems.PlaceOfStay(namePlace, start, end, position, description);
+        experience.setList(obj);
+    }
+
+    public DateTextItems getEducation() {
+        return education;
+    }
+
+    public void setEducation(String namePlace, LocalDate start, LocalDate end, String position) {
+        DateTextItems.PlaceOfStay obj = new DateTextItems.PlaceOfStay(namePlace, start, end, position, null);
+        education.setList(obj);
     }
 
     public String getFullName() {
