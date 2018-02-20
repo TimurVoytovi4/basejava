a3fdbf0a743f62c024473c7c2685992dc85d9245
TimurVoytovi4 <t.voytovi4@gmail.com>
2018-02-20 22:12:08 +0200

    Added fileStorage

--- src/webapp/exception/StorageException.java
+++ src/webapp/exception/StorageException.java
@@ -1,5 +1,7 @@
 package webapp.exception;
 
+import java.io.IOException;
+
 public class StorageException extends RuntimeException {
     private final String uuid;
 
@@ -8,4 +10,8 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
+    public StorageException(String message, String uuid, Exception e) {
+        super(message, e);
+        this.uuid = uuid;
+    }
 }
