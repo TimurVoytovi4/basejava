2f57ec903aa4f5e466c44fd069b9d5be10e49063
timur <t.voytovi4@gmail.com>
2018-04-12 16:51:33 +0300

    Made pretty output

--- src/webapp/MainFile.java
+++ src/webapp/MainFile.java
@@ -16,7 +16,7 @@ public class MainFile {
             throw new RuntimeException("Error", e);
         }
 
-        File dir = new File("/home/user/IdeaProjects/basejava");
+        File dir = new File("/home/timur/IdeaProjects/basejava");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
         if (list != null) {
@@ -29,6 +29,22 @@ public class MainFile {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        printDirectoryDeeply(dir);
+    }
+
+    private static void printDirectoryDeeply(File dir) {
+        File[] files = dir.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println("File: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println("Directory: " + file.getName());
+                    printDirectoryDeeply(file);
+                }
+            }
         }
     }
 }
--- src/webapp/RecursiveBypass.java
+++ src/webapp/RecursiveBypass.java
@@ -5,26 +5,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class RecursiveBypass {
-
+    private static StringBuilder indent = new StringBuilder("\t");
     private static void listAll(String path, ArrayList<String> res) throws IOException {
         File dir = new File(path);
         File[] list = dir.listFiles();
-
         if (list != null) {
             for (File f : list) {
-                if (f.isFile()) {
-                    res.add(f.getCanonicalPath());
-                } else {
+                if (f.isDirectory()) {
+                    res.add(indent + "Directory: " + f.getName());
+                    indent.append("\t");
                     listAll(f.getCanonicalPath(), res);
+                } else if (f.isFile()) {
+                    res.add(indent + "File: " + f.getName());
                 }
-            }
+            }indent.deleteCharAt(indent.indexOf("\t"));
         }
     }
 
     public static void main(String[] args) {
         ArrayList<String> res = new ArrayList<>();
-        final String path = "/home/user/IdeaProjects/basejava";
-
+        final String path = "/home/timur/IdeaProjects/basejava";
         try {
             listAll(path, res);
         } catch (IOException e) {
