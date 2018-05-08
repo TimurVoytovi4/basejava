ab68575b40dbb53e21d828d491cca87e8bf935c1
timur <t.voytovi4@gmail.com>
2018-05-09 01:58:31 +0300

    HW 11

--- src/webapp/Deadlock.java
+++ src/webapp/Deadlock.java
@@ -2,7 +2,16 @@ package webapp;
 
 public class Deadlock {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = Thread.currentThread();
-        thread.join();
+//        Thread.currentThread().join();
+        final Thread mainThread = Thread.currentThread();
+        Thread newThread = new Thread(() -> {
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });newThread.start();
+
+        mainThread.join();
     }
 }
