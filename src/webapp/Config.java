a073c160c1de1300a3e06399910d1a4b71b25d8d
timur <t.voytovi4@gmail.com>
2018-06-19 05:41:20 +0300

    L17

--- src/webapp/Config.java
+++ src/webapp/Config.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 public class Config {
 
-    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -19,13 +19,13 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties properties = new Properties();
             properties.load(is);
             storageDir = new File(properties.getProperty("storage.dir"));
             storage = new SqlStorage(properties.getProperty("db.url"), properties.getProperty("db.user"), properties.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file" + PROPS);
         }
     }
 
@@ -37,12 +37,5 @@ public class Config {
         return storageDir;
     }
 
-    private static File getHomeDir() {
-        String prop = System.getProperty("homeDir");
-        File homeDir = new File(prop == null ? "." : prop);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir + "is not directory");
-        }
-        return homeDir;
-    }
+
 }
--- web/WEB-INF/jsp/list.jsp
+++ web/WEB-INF/jsp/list.jsp
@@ -10,7 +10,7 @@
 <body>
 <jsp:include page="fragments/header.jsp"/>
 <section>
-    <a href="resume?action=add"><img src="img/add.png"></a>
+    <a href="resume?action=add"><img src="img/add.png" style="margin: auto"></a>
     <br>
     <table border="1" cellpadding="8" cellspacing="0" style="margin: auto">
         <tr>
